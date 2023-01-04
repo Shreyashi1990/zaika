35e33d5d76feae40538c7486cdc3c6e53e4ed5db
Naina Aggarwal <nainaaggarwal@192.168.1.10>
2023-01-04 14:23:51 +0530

    Pushing vendor folder

---
+++ core/vendor/_laravel_idea/_ide_helper_macro.php
@@ -0,0 +1,39 @@
+<?php
+/** @noinspection PhpUndefinedClassInspection */
+/** @noinspection PhpFullyQualifiedNameUsageInspection */
+
+namespace Illuminate\Http {
+    
+    /**
+     * @method $this domain(string $domain)
+     */
+    class RedirectResponse {}
+    
+    /**
+     * @method array validate(array $rules, ...$params)
+     * @method array validateWithBag(string $errorBag, array $rules, ...$params)
+     * @method bool hasValidSignature($absolute = true)
+     * @method bool hasValidRelativeSignature()
+     */
+    class Request {}
+}
+
+namespace Illuminate\Routing {
+    
+    /**
+     * @method $this role($roles = [])
+     * @method $this permission($permissions = [])
+     */
+    class Route {}
+}
+
+namespace Illuminate\Support\Facades {
+    
+    /**
+     * @method void emailVerification()
+     * @method void auth($options = [])
+     * @method void resetPassword()
+     * @method void confirmPassword()
+     */
+    class Route {}
+}
